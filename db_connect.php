742b86eba45161ba30161bc60a66d3f399c45b31
Thiri Winyati <thiriwinyati@Thiris-MacBook-Pro.local>
2026-05-12 21:09:01 +0100

    db connect update

--- db_connect.php
+++ db_connect.php
@@ -5,8 +5,6 @@ $password = getenv('DB_PASS') ?: '';
 $database = getenv('DB_NAME') ?: 'cosmetics_store';
 $port = getenv('DB_PORT') ?: 3306;
 
-die("DB_HOST is: " . $server);
-
 // PDO connection
 try {
     $conn = new PDO(
