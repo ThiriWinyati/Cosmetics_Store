49844f0b4317bb4a8df7daa523760060e1522bbc
Thiri Winyati <thiriwinyati@Thiris-MacBook-Pro.local>
2026-05-13 13:10:37 +0100

    Clean customer navbar CSS and collapse

--- Customer/navbar.php
+++ Customer/navbar.php
@@ -31,26 +31,7 @@ if (!empty($cartItems)) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="../Customer/customer_css/style.css">
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="icon" href="path/to/favicon.ico">
-    <title>Navbar</title>
-</head>
-
-<body>
-    
     <nav class="navbar navbar-expand-lg navbar-light bg-white customer-navbar">
         <div class="container-fluid">
 
@@ -195,21 +176,4 @@ if (!empty($cartItems)) {
             </div>
         </div>
     </nav>
-
-    <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            const navbarCollapse = document.getElementById("mainNavbar");
-
-            document.querySelectorAll("#mainNavbar .nav-link").forEach(function (link) {
-                link.addEventListener("click", function () {
-                    if (window.innerWidth < 992 && navbarCollapse.classList.contains("show")) {
-                        const bsCollapse = bootstrap.Collapse.getOrCreateInstance(navbarCollapse);
-                        bsCollapse.hide();
-                    }
-                });
-            });
-        });
-        </script>                        
-</body>
-
-</html>
+                      
